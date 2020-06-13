74938fe13d5de4573d3b02365bc93c601d045287
BidiznaGoginashvili <45213809+BidiznaGoginashvili@users.noreply.github.com>
2020-06-14 01:05:20 +0400

    Add project files.

---
+++ WCFTaskTwoService/IService1.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+
+namespace WCFTaskTwoService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int sum(int num1, int num2);
+
+        [OperationContract]
+        int Subtract(int num1, int num2);
+
+        [OperationContract]
+        int Multiply(int num1, int num2);
+
+        [OperationContract]
+        int Divide(int num1, int num2);
+    }
+}
